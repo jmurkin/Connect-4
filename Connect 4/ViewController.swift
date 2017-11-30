37128e13946a94f9db1cdc6cd48cf1354cc24511
Jake Murkin <mac@imac-de-mac.home>
2017-11-30 14:27:49 +0000

    Initial Commit

---
+++ Connect 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Connect 4
+//
+//  Created by Mac on 30/11/2017.
+//  Copyright © 2017 Jake Murkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
